7ff3263578249f5d32f6fe6ad1e9e1a3e4f776ff
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-15 16:12:36 +0100

    added app header component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+    name: 'App',
+    components: { AppHeader }
+};
+
+</script>
+
+<template>
+    <app-header></app-header>
+</template>
+
+<style></style>
