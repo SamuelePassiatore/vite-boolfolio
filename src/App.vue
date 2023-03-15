175455cbc5aa7e08fff2230dda542b697ba48307
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-15 16:19:00 +0100

    added data projects, fetch projects and api base url

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+const apiBaseUrl = 'http://localhost:8000/api';
 export default {
     name: 'App',
-    components: { AppHeader }
+    components: { AppHeader },
+    data: () => ({
+        projects: []
+    }),
+    methods: {
+        fetchProjects() {
+            axios.get('')
+        }
+    }
 };
 
 </script>
 
 <template>
     <app-header></app-header>
+    <main class="container">
+        <ul>
+            <li></li>
+        </ul>
+    </main>
 </template>
 
 <style></style>
