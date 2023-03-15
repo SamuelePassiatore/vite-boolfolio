c9e75afd34f089a8ebbd056644cc4a2b7817d72e
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-15 23:39:55 +0100

    added alert with app alert component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import AppAlert from './components/AppAlert.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
-const apiBaseUrl = 'http://localhost:8000/api';
+const apiBaseUrl = 'h://localhost:8000/api';
 export default {
     name: 'Boolfolio',
-    components: { AppHeader, ProjectsList },
+    components: { AppHeader, AppAlert, ProjectsList },
     data: () => ({
         isLoading: false,
+        hasError: false,
         projects: []
     }),
     methods: {
@@ -15,8 +17,8 @@ export default {
             this.isLoading = true;
             axios.get(apiBaseUrl + '/projects').then(res => {
                 this.projects = res.data;
-            }).catch((err) => {
-                console.error(err);
+            }).catch(() => {
+                this.hasError = true;
             }).then(() => {
                 this.isLoading = false;
             });
@@ -32,6 +34,7 @@ export default {
 <template>
     <app-header></app-header>
     <main class="container">
+        <app-alert :is-open="hasError" @close="hasError = false"></app-alert>
         <app-loader v-if="isLoading"></app-loader>
         <projects-list v-else :projects="projects"></projects-list>
     </main>
---
+++ src/components/AppAlert.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppAlert',
+    props: { isOpen: Boolean },
+    emits: ['close']
+};
+
+</script>
+
+<template>
+    <div v-if="isOpen" class="alert alert-danger alert-dismissible fade show my-3" role="alert">
+        <strong>Error! </strong> An error occurred in the recovery of projects!
+        <button type="button" class="btn-close" @click="$emit('close')"></button>
+    </div>
+</template >
+
+<style scoped lang ="scss"></style>
