3b11cbf94ee03b882d38af3c527da6c5af1a9c08
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-15 17:21:21 +0100

    added project card and print cards in page

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: { project: Object },
+};
+
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <div>
+            <img :src="project.image" class="img-fluid w-50 " :alt="project.name">
+        </div>
+        <div class="card-header">
+            <h5 class="card-title">{{ project.title }}</h5>
+        </div>
+        <div class="card-body">
+            <h6 class="card-subtitle mb-2 text-muted">{{ project.slug }}</h6>
+            <p class="card-text">{{ project.description }}</p>
+        </div>
+        <div class="card-footer">
+            <div>
+                <small>{{ project.author.name }}</small>
+            </div>
+            <small><time>{{ project.updated_at }}</time></small>
+        </div>
+    </div>
+</template>
+
+<style></style>
--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,6 +1,8 @@
 <script>
+import ProjectCard from './ProjectCard.vue';
 export default {
     name: 'ProjectsList',
+    components: { ProjectCard },
     props: { projects: Array },
 };
 
@@ -8,9 +10,9 @@ export default {
 
 <template>
     <section id="projects-list" class="my-4">
-        <ul v-if="projects.length">
-            <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
-        </ul>
+        <div v-if="projects.length">
+            <project-card v-for="project in projects" :key="project.id" :project="project"></project-card>
+        </div>
         <h5 v-else class="text-center">There aren't projects in this portfolio</h5>
     </section>
 </template>
